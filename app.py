851d3c3a44abafa7a5491252c9c8b75c56417dca
Elyire <pwoc517@gmail.com>
2023-09-25 23:56:40 +0900

    slide show

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
 # 공유된 구글 슬라이드의 링크
-slide_url = "https://docs.google.com/presentation/d/1xzdkFx6__bZ-acvcjaeGY5GbCzBs0dqFvGIC8QTDRRM/edit?usp=sharing"
+slide_url = "https://docs.google.com/presentation/d/e/2PACX-1vR0kPLGtR_nxqoB1srjsSxuSSyPcMt3T8fWuVJJF4FVNi6xk_7u36xRy_XHaZMPQq9iad_ZooFwvFUa/embed?start=false&loop=false&delayms=3000"
 
 # Streamlit 앱에 제목 추가
 st.title('GPT 동작 원리 개론')
 
 
 # 슬라이드를 임베드합니다.
-st.markdown(f'<iframe src="{slide_url}" width="700" height="400"></iframe>', unsafe_allow_html=True)
+st.markdown(f'<iframe src="{slide_url}" frameborder="0" width="1440" height="839" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>', unsafe_allow_html=True)
 
 
