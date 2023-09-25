17e222f6f04cf31798e13ecd36220c4fa445fe75
Elyire <pwoc517@gmail.com>
2023-09-26 02:12:19 +0900

    test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ slide_url = "https://docs.google.com/presentation/d/e/2PACX-1vR0kPLGtR_nxqoB1srj
 google_slide_direct_link = "https://docs.google.com/presentation/d/1xzdkFx6__bZ-acvcjaeGY5GbCzBs0dqFvGIC8QTDRRM/edit?usp=sharing"  
 
 # Streamlit 앱에 제목 추가
-st.header('GPT 동작 원리 개론')
+st.header("GPT 동작 원리 개론")
 st.subheader("GPT의 트랜스포머 모델(Transformer model)을 살펴봅니다")
 st.write("Write Something")
 st.markdown(f"여기에는 어떤 것이 들어오면 좋을까요?", unsafe_allow_html=True)
@@ -16,4 +16,3 @@ st.markdown(f'<iframe src="{slide_url}" frameborder="0" width="960" height="569"
 
 # 사용자에게 전체 화면으로 보기를 권장하는 메시지와 링크 제공
 #st.markdown(f"전체 화면으로 슬라이드쇼를 보려면 [여기]( {google_slide_direct_link} )를 클릭하세요.", unsafe_allow_html=True)
-st.font = "Noto Sans KR"
