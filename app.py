34220c00d658f18e865ff5bbb91e0157928ec23c
Elyire <pwoc517@gmail.com>
2023-09-27 13:09:13 +0900

    pos

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ st.markdown(
     <style>
         iframe[width="220"] {
             position: fixed;
-            bottom: 200px;
+            bottom: 150px;
             right: 200px;
         }
     </style>
