91855b200304d3233b5a0f22617b46081d03868f
Elyire <pwoc517@gmail.com>
2023-10-05 14:08:27 +0900

    banner position

--- app.py
+++ app.py
@@ -38,10 +38,10 @@ st.header("디코더 유형 트랜스포머 아키텍처 개론")
 st.subheader("GPT와 같은 디코더 유형 트랜스포머 모델을 살펴봅니다")
 
 
-# 슬라이드 임베드(상대 위치 사용)
+# 부모 컨테이너의 최소 높이 설정
 st.markdown(
     """
-    <div style="position: relative; width: 960px; height: 639px;"> <!-- 배너의 높이(70px)를 추가한 높이로 설정 -->
+    <div style="position: relative; width: 960px; min-height: 639px;"> <!-- 배너의 높이(70px)를 추가한 높이로 설정 -->
         <iframe src="{slide_url}" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
         <iframe width="220" style="position: absolute; bottom: 0; right: 10px;">
             <!-- 배너 내용 -->
@@ -50,6 +50,37 @@ st.markdown(
     """.format(slide_url=slide_url),
     unsafe_allow_html=True,
 )
+
+# 미디어 쿼리를 사용하여 화면 높이에 따라 스타일 적용
+st.markdown(
+    """
+    <style>
+        @media (max-height: 700px) {
+            iframe[width="220"] {
+                position: static; /* 배너의 위치를 기본 값으로 설정 */
+                display: block;
+                margin: 10px auto; /* 중앙 정렬 */
+            }
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+
+
+# # 슬라이드 임베드(상대 위치 사용)
+# st.markdown(
+#     """
+#     <div style="position: relative; width: 960px; height: 639px;"> <!-- 배너의 높이(70px)를 추가한 높이로 설정 -->
+#         <iframe src="{slide_url}" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+#         <iframe width="220" style="position: absolute; bottom: 0; right: 10px;">
+#             <!-- 배너 내용 -->
+#         </iframe>
+#     </div>
+#     """.format(slide_url=slide_url),
+#     unsafe_allow_html=True,
+# )
 
 # 슬라이드를 임베드합니다..
 # st.markdown(f'<iframe src="{slide_url}" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>', unsafe_allow_html=True)
@@ -64,7 +95,7 @@ button = """
 <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="rdjhyoonc" data-color="#FFDD00" data-emoji="☕"  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>
 """
 
-html(button, height=70, width=220)
+# html(button, height=70, width=220)
 
 st.markdown(button, unsafe_allow_html=True)
 # st.markdown(
